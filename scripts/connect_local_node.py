ad6a2ccd42fcea3b478ef14d1fc91cfa83bd6244
data-miner00 <data-miner00@users.noreply.github.com>
2022-06-09 03:14:04 +0800

    ✏️ Fix wrong env name

--- scripts/connect_local_node.py
+++ scripts/connect_local_node.py
@@ -6,7 +6,7 @@ algod_address = "http://localhost:4001"
 algod_token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 algod_client = algod.AlgodClient(algod_token, algod_address)
 
-my_address = os.getenv("ALGORAND_WALLET_PUBLIC_ADDRESS")
+my_address = os.getenv("ALGORAND_ACCOUNT_FIRST_ADDRESS")
 
 account_info = algod_client.account_info(my_address)
 print(account_info)
